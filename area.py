21c95a261926fcf0148ce74124ea2713b037f20b
Manju Dubey <manjudube@gmail.com>
2020-11-08 19:19:22 -0500

    right a function

---
+++ area.py
@@ -0,0 +1,12 @@
+
+
+import math
+def main():
+    radius = eval(input(' enter a radius number.'))
+    pi = 3.14
+    area = pi * radius ** 2
+    for i in range(5):
+        radius= float(input('enter a radus number'))
+    area = pi * radius ** 2
+    print('The area of the circle ')
+main()
